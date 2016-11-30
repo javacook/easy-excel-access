c038365f0e0be4c69f36c59a59b958765243e2a0
Joerg Vollmer <J.Vollmer@verband.creditreform.de>
2016-11-30 08:59:18 +0100

    Print out two more cells

--- src/test/java/com/javacook/easyexcellaccess/ExcelAccessorMain.java
+++ src/test/java/com/javacook/easyexcellaccess/ExcelAccessorMain.java
@@ -10,12 +10,13 @@ import java.io.IOException;
 public class ExcelAccessorMain {
 
     public static void main(String[] args) throws IOException {
-        ExcelAccessor excelAccessor = new ExcelAccessor("/zwei.xls");
+        ExcelAccessor excelAccessor = new ExcelAccessor("zwei.xls");
         System.out.println(excelAccessor.readCell(0, 0, 2, String.class));
+        System.out.println(excelAccessor.readCell(0, 0, 2));
+        System.out.println(excelAccessor.readCell(0, 1, 2));
         System.out.println(excelAccessor.readCell(0, 1, 0, Integer.class));
         System.out.println(excelAccessor.noRows(0));
         System.out.println(excelAccessor.noCols(0));
-
     }
 
 }
