4f1a65174065e74e5057ae120f2b9053242d569b
javacook <javacook@gmx.de>
2016-12-06 10:16:05 +0100

    Neue Methode readCell

--- src/main/java/com/javacook/easyexcelaccess/ExcelEasyAccess.java
+++ src/main/java/com/javacook/easyexcelaccess/ExcelEasyAccess.java
@@ -4,10 +4,22 @@ package com.javacook.easyexcelaccess;
  * Base routines to access Excel content.
  */
 public interface ExcelEasyAccess {
+	
+    /**
+     * Reads the content of the cell at the coordintes <code>x</code> and <code>y</code>
+     * from the sheet with no <code>sheet</code>. The type of the Excel cell content is 
+     * automatically detected and the class of the result is respecting String, Number, 
+     * Boolean or Date.
+     * @param sheet Excel sheet number starting with 0
+     * @param x column coordinate starting with 0
+     * @param y row coordinate starting with 0
+     * @return cell content
+     */
+    Object readCell(int sheet, int x, int y);
 
     /**
      * Reads the content of the cell at the coordintes <code>x</code> and <code>y</code>
-     * in the sheet with no <code>sheet</code>.
+     * from the sheet with no <code>sheet</code>.
      *
      * @param sheet Excel sheet number starting with 0
      * @param x column coordinate starting with 0
@@ -21,12 +33,14 @@ public interface ExcelEasyAccess {
     /**
      * Maximal row number containing real data plus 1
      * @param sheet Excel sheet number starting with 0
+     * @return Row size of bounding box containing real data
      */
     int noRows(int sheet);
 
     /**
      * Maximal row column number containing real data plus 1
      * @param sheet Excel sheet number starting with 0
+     * @return Column size of bounding box containing real data
      */
     int noCols(int sheet);
 }
